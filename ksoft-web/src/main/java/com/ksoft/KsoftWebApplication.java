7410565e207a6c4cdc21d0dc39d0839959211c6f
张建波 <k963@vip.qq.com>
2017-03-28 14:35:20 +0800

    初始提交
    Signed-off-by: 张建波 <k963@vip.qq.com>

---
+++ ksoft-web/src/main/java/com/ksoft/KsoftWebApplication.java
@@ -0,0 +1,12 @@
+package com.ksoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsoftWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsoftWebApplication.class, args);
+	}
+}
